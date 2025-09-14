65a0cef3b4ddd53f6df2b70426a8ba447c577d63
rajukarki467 <rajukarki467@gmail.com>
2025-09-14 10:00:54 +0545

    making responsiv navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:my_portfolio/constants/colors.dart';
+import 'package:my_portfolio/constants/sized.dart';
 import 'package:my_portfolio/widgets/drawer_mobile.dart';
+import 'package:my_portfolio/widgets/header_dasktop.dart';
 import 'package:my_portfolio/widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,40 +17,48 @@ class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      endDrawer: const DrawerMobile(),
-      backgroundColor: CustomColor.ScaffoldBg,
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          //MAIN
-          // HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: () {},
-            onMenuTap: () {
-              scaffoldKey.currentState?.openEndDrawer();
-            },
-          ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          key: scaffoldKey,
+          endDrawer: constraints.maxWidth >= kMinDesktopWidth
+              ? null
+              : const DrawerMobile(),
+          backgroundColor: CustomColor.ScaffoldBg,
+          body: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              //MAIN
+              if (constraints.maxWidth >= kMinDesktopWidth)
+                const HeaderDesktop()
+              else
+                HeaderMobile(
+                  onLogoTap: () {},
+                  onMenuTap: () {
+                    scaffoldKey.currentState?.openEndDrawer();
+                  },
+                ),
 
-          //SKILL
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
+              //SKILL
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+              //PROJECTS
+              Container(height: 500, width: double.maxFinite),
+              //CONTACT
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+              //FOOTER
+              Container(height: 500, width: double.maxFinite),
+            ],
           ),
-          //PROJECTS
-          Container(height: 500, width: double.maxFinite),
-          //CONTACT
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-          //FOOTER
-          Container(height: 500, width: double.maxFinite),
-        ],
-      ),
+        );
+      },
     );
   }
 }
