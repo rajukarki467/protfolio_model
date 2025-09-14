fe125978136f0198919a876f25b4d752e6cf8e7d
rajukarki467 <rajukarki467@gmail.com>
2025-09-14 15:06:48 +0545

    making list of platform and skill sets

---
+++ lib/constants/skill_items.dart
@@ -0,0 +1,15 @@
+const List<Map> platformItems = [
+  {"img": "assets/android_icon.png", "title": "Android Dev"},
+  {"img": "assets/web_icon.png", "title": "Web Dev"},
+  {"img": "assets/iso_icon.png", "title": "ISO Dev"},
+  {"img": "assets/desktop_icon.png", "title": "Desktop Dev"},
+];
+
+const List<Map> skillItems = [
+  {"img": "assets/flutter.png", "title": "Flutter"},
+  {"img": "assets/dart.png", "title": "Dart"},
+  {"img": "assets/html5.png", "title": "HTML5"},
+  {"img": "assets/ccs3.png", "title": "CSS3"},
+  {"img": "assets/javaScript.png", "title": "JS"},
+  {"img": "assets/hugo.png", "title": "Hugo"},
+];
